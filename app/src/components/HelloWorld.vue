59fb1add1af8f3c3770276c57c55bfe98ddeceba
Rizky Ajie Kurniawan - Mac <rizkyak994@gmail.com>
2023-10-17 03:12:05 +0700

    added bootstrap

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <b-button variant="danger">Button</b-button>
+
+    <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </b-card-text>
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the

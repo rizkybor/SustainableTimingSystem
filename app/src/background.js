8e197ad4e38385dbccfc8442840392af38cdea97
Rizky Ajie Kurniawan - Mac <rizkyak994@gmail.com>
2024-04-11 14:30:44 +0700

    update splash screen

--- app/src/background.js
+++ app/src/background.js
@@ -10,84 +10,61 @@ import path from 'path';
 // Function to Services Communication to Database
 setupIPCMainHandlers()
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-let loadingWindow;
-
-function createLoadingWindow() {
-  loadingWindow = new BrowserWindow({
-    width: 300,
-    height: 100,
-    frame: false,
-    transparent: false,
-    alwaysOnTop: false,
-    resizable: false,
-    webPreferences: {
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
-    }
-  });
-  loadingWindow.loadURL(`file://${path.join(__dirname, '../public/loading.html')}`);
-}
-
 async function createWindow() {
-  // Create the browser window.
   const win = new BrowserWindow({ 
     width: 1200,
     height: 800,
+    show: false,
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
   })
 
   win.loadFile('index.html')
+  var splash = new BrowserWindow({ 
+    width: 500, 
+    height: 200, 
+    transparent: true, 
+    frame: false, 
+    alwaysOnTop: false 
+  });
+  splash.loadFile(path.join(__dirname, '../public/splash.html'));
+  splash.center();
+  setTimeout(function () {
+    splash.close();
+    win.center();
+    win.show();
+  }, 5000);
+
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
-    // Load the index.html when not in development
     win.loadURL('app://./index.html')
-  }
-
-   // Close the loading window
-   if (loadingWindow) {
-    loadingWindow.close();
-    loadingWindow = null;
   }
 }
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  // if (BrowserWindow.getAllWindows().length === 0) createWindow() 
   if (BrowserWindow.getAllWindows().length === 0) {
-    createLoadingWindow();
-    setTimeout(createWindow, 1000)
+    createWindow();
   }
 
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
@@ -97,12 +74,9 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
-  createLoadingWindow();
-  setTimeout(createWindow, 2000)
-  // createWindow()
+  createWindow()
 })
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
